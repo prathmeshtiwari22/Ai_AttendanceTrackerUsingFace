c23a40550ecec675156d2671fb55673bed58037c
prathmeshtiwari22 <122382508+prathmeshtiwari22@users.noreply.github.com>
2024-03-06 20:24:26 +0530

    try

---
+++ src/AppNavigator.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './components/Home';
+
+const AppNavigator = () => {
+    const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default AppNavigator
+
+const styles = StyleSheet.create({})
